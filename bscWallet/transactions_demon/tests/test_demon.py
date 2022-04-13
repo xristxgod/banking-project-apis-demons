d49d7130154a2d3e20f7fb05acf64a2cbddefaa3
Murad Mamedov <84931791+xristxgod@users.noreply.github.com>
2022-04-13 19:21:55 +0300

    Update test_demon.py
    [FIX] test_demon.py

--- bscWallet/transactions_demon/tests/test_demon.py
+++ bscWallet/transactions_demon/tests/test_demon.py
@@ -1,38 +1,17 @@
-"""
-This test is used to check the operability of the database and the method of accessing it.
-# Check everything
-python -m unittest test_demon.TestDemon
-# Check receipt of all addresses
-python -m unittest test_demon.TestDemon.test_get_addresses
-# Check receipt of all transaction hash
-python -m unittest test_demon.TestDemon.test_get_all_transactions_hash
-# Check receipt of all tokens from the database
-python -m unittest test_demon.TestDemon.test_get_contracts
-"""
-import unittest
-import random
-
+from unittest import IsolatedAsyncioTestCase
 from src.external_data.database import DB
 
 
-class TestDemon(unittest.IsolatedAsyncioTestCase):
+class TestDemon(IsolatedAsyncioTestCase):
     async def test_get_addresses(self):
         addresses = await DB.get_addresses()
-        self.assertEqual(type(addresses), list)
-        self.assertIsNot(addresses, [])
-        self.assertIsNotNone(addresses)
-        self.assertIn(addresses[random.randint(0, len(addresses))][0].upper(), ["T", "4"])
+        assert isinstance(addresses, list)
 
     async def test_get_all_transactions_hash(self):
         tx_hash = await DB.get_all_transactions_hash()
-        self.assertIsNotNone(tx_hash)
-        self.assertEqual(type(tx_hash), list)
-        if len(tx_hash) > 0:
-            tx = DB.get_transaction_by_hash(transaction_hash=tx_hash[0])
-            self.assertEqual(type(tx), dict)
+        assert tx_hash is not None
+        assert isinstance(tx_hash, list)
 
     def test_get_contracts(self):
         contracts = DB.get_tokens()
-        self.assertEqual(type(contracts), list)
-        for contract in contracts:
-            self.assertIn(contract["symbol"], ["USDT", "USDC", "RZM", "DNC", "MATIC", "LINK"])
+        assert isinstance(contracts, list)
