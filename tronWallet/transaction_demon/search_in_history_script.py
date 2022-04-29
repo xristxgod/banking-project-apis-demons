6510c670332e04c54f93da51c8f6737cd95d96f8
Murad Mamedov <84931791+xristxgod@users.noreply.github.com>
2022-04-29 13:42:47 +0300

    [FIX] Unclosed AsyncClient

--- tronWallet/transaction_demon/search_in_history_script.py
+++ tronWallet/transaction_demon/search_in_history_script.py
@@ -18,11 +18,19 @@ async def async_run(**kwargs):
     logger.error(f"Creating demon instance. Network: {network}")
     logger.error(f"Start search in history")
     if "list_addresses" in kwargs and kwargs["list_addresses"] is not None:
-        demon = AddressesDemon()
-        is_success = await demon.start(**kwargs)
+        try:
+            demon = AddressesDemon()
+            is_success = await demon.start(**kwargs)
+            await demon.node.close()
+        except Exception:
+            is_success = True
     else:
-        demon = TransactionDemon()
-        is_success = await demon.start(**kwargs)
+        try:
+            demon = TransactionDemon()
+            is_success = await demon.start(**kwargs)
+            await demon.node.close()
+        except Exception:
+            is_success = True
     logger.info(f"Script ran success: {is_success}")
 
 if __name__ == '__main__':
