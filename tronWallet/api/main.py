3e4cedfea2326c5e7f7b6abf3fb26e9a1d5111bd
Mamedov Murad <mamedov_99b@mail.ru>
2022-08-18 11:12:47 +0300

    [UPD] new start file app => main & new format

---
+++ tronWallet/api/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from config import Config
+
+
+app = FastAPI(
+    title=f"TronNetwork '{Config.NETWORK}'",
+    description="Service for interacting with the Tron network.",
+    version="1.0.0",
+)
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run("app:app")
