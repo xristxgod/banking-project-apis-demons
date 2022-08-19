ef7403cfc1690dead64841653542896353544e76
Mamedov Murad <mamedov_99b@mail.ru>
2022-08-19 15:58:51 +0300

    [ADD] new module for endpoints

--- tronWallet/api/app/__init__.py
+++ tronWallet/api/app/__init__.py
@@ -1,4 +1,11 @@
 from fastapi import APIRouter
+from .endpoints import admin, status, transaction, wallet
 
 
 router = APIRouter()
+
+
+router.include_router(admin.router)         # Router for admin
+router.include_router(wallet.router)        # Router for work wallet
+router.include_router(transaction.router)   # Router for work transaction
+router.include_router(status.router)        # Router for check status
