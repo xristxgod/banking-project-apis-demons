12b8ed04ba501c45ab42fac24a79757660022525
Mamedov Murad <mamedov_99b@mail.ru>
2022-08-25 15:23:18 +0300

    [ADD] schemas | dataclass in daemon => schemas

---
+++ tronWallet/transaction_demon/src/schemas.py
@@ -0,0 +1,62 @@
+import decimal
+from dataclasses import dataclass, field
+from typing import Optional, List, Dict
+
+from tronpy.tron import TAddress
+
+
+@dataclass()
+class ProcessingTransaction:
+    transaction: Dict                               # Transaction Data
+    addresses: List[TAddress]                       # Wallet addresses in Database
+    timestamp: int                                  # Time to send transaction
+    transactionsHash: List[str]                     # Transactions in Database
+
+
+@dataclass()
+class SmartContractData:
+    amount: decimal.Decimal                         # Transaction amount
+    symbol: str                                     # Token name
+
+
+@dataclass()
+class Participant:
+    address: TAddress                               # Participant wallet address
+    amount: decimal.Decimal                         # Transaction amount
+
+
+@dataclass()
+class Transaction:
+    timestamp: int                                  # Time to send transaction
+    transactionHash: str                            # Transaction hash/id
+    amount: decimal.Decimal                         # Transaction amount
+    fee: decimal.Decimal                            # Transaction fee
+    inputs: List[Participant]                       # Sender transaction
+    outputs: List[Participant]                      # Recipient transaction
+    token: Optional[str] = field(default=None)      # Token symbol
+
+
+@dataclass()
+class BodyTransaction:
+    address: TAddress                               # Participant transaction in has to db
+    transactions: List[Transaction]                 # Transaction list
+
+
+@dataclass()
+class SendTransactionData:
+    transactionPackage: BodyTransaction             # Transactions for send
+    addresses: List[TAddress]                       # Wallet addresses in Database
+    transactionsHash: List[str]                     # Transactions in Database
+    blockNumber: int                                # Block in Blockchain
+
+
+@dataclass()
+class Header:
+    block: int                                      # Block in Blockchain
+    network: str                                    # Example: tron, tron_trc20_usdt, tron_trc20_usdc, etc.
+
+
+__all__ = [
+    "ProcessingTransaction", "SmartContractData", "Participant",
+    "Transaction", "BodyTransaction", "SendTransactionData", "Header"
+]
