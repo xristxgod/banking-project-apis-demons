f13f6fdab4f09b4deb9c2c01574306db21237e0a
Mamedov Murad <mamedov_99b@mail.ru>
2022-08-25 11:28:45 +0300

    [ADD] new format for core

--- tronWallet/transaction_demon/src/core.py
+++ tronWallet/transaction_demon/src/core.py
@@ -1,9 +1,11 @@
-from typing import Optional, Dict
+from typing import Optional, Callable
 from dataclasses import dataclass, field
 
+import tronpy.exceptions
 from tronpy.async_tron import AsyncTron, AsyncHTTPProvider
 
-from config import Config
+from .external import ElasticController
+from config import Config, logger
 
 
 PUBLIC_TRON_GRID_KEY = "16c3b7ca-d498-4314-aa1d-a224135faa26"
@@ -36,6 +38,18 @@ class Core:
             await self.__node.close()
             del self.__node
 
+    def __getattr__(self, method) -> Optional[Callable]:
+        try:
+            if not hasattr(self.node, method):
+                return None
+            return getattr(self.node, method)
+        except tronpy.exceptions.ApiError:
+            logger.error("Something is wrong on the sending side!")
+            self.update()
+        except tronpy.exceptions.BugInJavaTron:
+            logger.error("Something is wrong on the server for years!")
+            self.update()
+
     @property
     def network(self) -> str:
         return Config.NETWORK
@@ -55,6 +69,8 @@ class Core:
 
     def update(self, iterations: int = 20) -> Optional:
         if self.__temporary is None:
+            logger.error(f"Problems with the main node! Check its performance!")
+            await ElasticController.send_error(message="Problems with the main note! Check its performance!", code=1)
             self.__temporary = Temporary(
                 node=AsyncTron(
                     provider=AsyncHTTPProvider(api_key=PUBLIC_TRON_GRID_KEY) if Config.NETWORK.upper() == "MAINNET" else None,
@@ -63,4 +79,5 @@ class Core:
                 iterations=iterations
             )
         else:
+            logger.error(f"The main node is still not working! Adding {iterations} more iterations")
             self.__temporary.iterations += 20
