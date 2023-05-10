5fe3f55a92372ddfcc78566624d99d75c51d7555
Mamedov Murad <mamedov_99b@mail.ru>
2023-05-10 15:08:45 +0300

    [ADD] auth

--- tron_network/core/crypto/models.py
+++ tron_network/core/crypto/models.py
@@ -2,8 +2,8 @@ from tortoise import models, fields
 
 
 class Contract(models.Model):
-    address = fields.CharField(max_length=50)
-    symbol = fields.CharField(max_length=10)
+    address = fields.CharField(max_length=50, unique=True)
+    symbol = fields.CharField(max_length=10, unique=True)
     name = fields.CharField(max_length=25)
     decimal_place = fields.IntField(default=8)
 
