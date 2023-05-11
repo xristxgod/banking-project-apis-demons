6e7130fac980dc51c6733dfce284db8e610a5f53
Mamedov Murad <mamedov_99b@mail.ru>
2023-05-11 11:26:07 +0300

    [ADD] new apps: transaction

--- tron_network/apps/common/endpoints.py
+++ tron_network/apps/common/endpoints.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Query, status
+from fastapi import APIRouter, status
 from fastapi.exceptions import HTTPException
 
 from core.crypto.calculator import FEE_METHOD_TYPES
@@ -30,49 +30,3 @@ async def wallet_balance(body: schemas.BodyWalletBalance):
 )
 async def allowance(body: schemas.BodyAllowance):
     return await services.allowance(body)
-
-
-@router.post(
-    '/transfer/create',
-    response_model=schemas.ResponseCreateTransaction
-)
-async def create_transfer(body: schemas.BodyCreateTransfer):
-    try:
-        return await services.CreateTransfer.create(body)
-    except Exception as err:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=str(err)
-        )
-
-
-@router.post(
-    '/approve/create',
-    response_model=schemas.ResponseCreateTransaction,
-)
-async def create_approve(body: schemas.BodyCreateApprove):
-    return await services.CreateApprove.create(body)
-
-
-@router.post(
-    '/transfer-from/create',
-    response_model=schemas.ResponseCreateTransaction
-)
-async def create_transfer_from(body: schemas.BodyCreateTransferFrom):
-    pass
-
-
-@router.post(
-    '/transaction/send',
-    response_model=schemas.ResponseSendTransaction,
-)
-async def send_transaction(body: schemas.BodySendTransaction):
-    return await services.SendTransaction.send_transaction(body)
-
-
-@router.post(
-    '/fee/{method}/calculate',
-    response_model=schemas.ResponseCommission,
-)
-async def fee_calculator(body: schemas.BodyCommission, method: FEE_METHOD_TYPES = FEE_METHOD_TYPES.TRANSFER):
-    return await services.fee_calculator(body, method=method)
--- tron_network/apps/common/schemas.py
+++ tron_network/apps/common/schemas.py
@@ -1,14 +1,10 @@
-import json
 import decimal
 from typing import Optional
 
-from tronpy.tron import TAddress, PrivateKey
-from tronpy.async_tron import AsyncTransaction
+from tronpy.tron import TAddress
 from pydantic import BaseModel, Field, validator
 
-import settings
 from core.crypto import node
-from core.crypto.utils import to_sun
 from core.crypto.contract import Contract
 from apps.common import utils
 
@@ -76,113 +72,3 @@ class BodyAllowance(CurrencyMixin, BaseModel):
 
 class ResponseAllowance(BaseModel):
     amount: decimal.Decimal = Field(default=0)
-
-
-class BodyCreateTransfer(CurrencyMixin, BaseModel):
-    from_address: TAddress
-    to_address: TAddress
-    amount: decimal.Decimal
-
-    currency: str = Field(default='TRX')
-
-    fee_limit: int = Field(default=settings.DEFAULT_FEE_LIMIT)
-
-    @property
-    def sun_amount(self) -> int:
-        # Amount to Sun amount
-        return to_sun(self.amount)
-
-    @validator('from_address', 'to_address')
-    def correct_address(cls, address: TAddress):
-        return utils.correct_address(address)
-
-
-class BodyCreateApprove(CurrencyMixin, BaseModel):
-    owner_address: TAddress
-    spender_address: TAddress
-    amount: decimal.Decimal
-
-    currency: str
-
-    fee_limit: int = Field(default=settings.DEFAULT_FEE_LIMIT)
-
-
-class BodyCreateTransferFrom(CurrencyMixin, BaseModel):
-    owner_address: TAddress
-    from_address: TAddress
-    to_address: TAddress
-    amount: decimal.Decimal
-
-    currency: str
-
-    fee_limit: int = Field(default=settings.DEFAULT_FEE_LIMIT)
-
-
-class ResponseCommission(BaseModel):
-    fee: decimal.Decimal = Field(default=0)
-    energy: int = Field(default=0)
-    bandwidth: int = Field(default=0)
-
-
-class ResponseCreateTransaction(BaseModel):
-    payload: str
-    commission: ResponseCommission
-
-    @property
-    def payload_dict(self) -> dict:
-        return json.loads(self.payload)
-
-
-class BodySendTransaction(BaseModel):
-    payload: str
-    private_key: str
-
-    @property
-    def payload_dict(self) -> dict:
-        return json.loads(self.payload)
-
-    async def create_transaction_obj(self) -> AsyncTransaction:
-        data = self.payload_dict.get('data')
-        return await AsyncTransaction.from_json(data, client=node.client)
-
-    @property
-    def private_key_obj(self):
-        return PrivateKey(private_key_bytes=bytes.fromhex(self.private_key))
-
-    @property
-    def extra(self) -> dict:
-        return self.payload_dict.get('extra')
-
-
-class ResponseSendTransactionExtra(BaseModel):
-    type: str
-    owner_address: Optional[TAddress] = None
-
-
-class ResponseSendTransaction(BaseModel):
-    transaction_id: str
-    timestamp: int
-    amount: decimal.Decimal
-    fee: decimal.Decimal
-    from_address: TAddress
-    to_address: TAddress
-    currency: str = Field(default='TRX')
-    extra: ResponseSendTransactionExtra
-
-
-class BodyCommission(BaseModel):
-    """
-    Parameter schemas:
-        Transfer:
-            from_address
-            to_address
-            amount
-            currency
-        Approve:
-            owner_address
-            spender_address
-            amount
-            currency
-    """
-
-    parameter: dict
