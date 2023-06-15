8c7a932d6def8671ceba03357941476dec93102d
Mamedov Murad <mamedov_99b@mail.ru>
2023-06-15 13:35:29 +0300

    [ADD] new apps: orders

--- backend/apps/telegram/bot.py
+++ backend/apps/telegram/bot.py
@@ -1,6 +1,12 @@
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 import settings
+from apps.telegram.bot_apps import init_all_handlers
 
 app = Bot(token=settings.TELEGRAM_BOT_TOKEN)
-dp = Dispatcher(app)
+storage = MemoryStorage()
+dp = Dispatcher(app, storage=storage)
+
+# Init bot apps
+init_all_handlers(dp)
