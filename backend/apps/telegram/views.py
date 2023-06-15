c544110753f8af3285424c66f4bc4afe12023e6c
Mamedov Murad <mamedov_99b@mail.ru>
2023-06-15 15:24:31 +0300

    [ADD]

--- backend/apps/telegram/views.py
+++ backend/apps/telegram/views.py
@@ -9,18 +9,18 @@ from apps.telegram.bot import app, dp
 router = APIRouter()
 
 
-# @router.on_event('startup')
-# async def startup():
-#     webhook_info = await app.get_webhook_info()
-#     if webhook_info.url != settings.TELEGRAM_WEBHOOK_URL:
-#         await app.set_webhook(
-#             url=settings.TELEGRAM_WEBHOOK_URL
-#         )
-#
-#
-# @router.on_event("shutdown")
-# async def shutdown():
-#     await app.session.close()
+@router.on_event('startup')
+async def startup():
+    webhook_info = await app.get_webhook_info()
+    if webhook_info.url != settings.TELEGRAM_WEBHOOK_URL:
+        await app.set_webhook(
+            url=settings.TELEGRAM_WEBHOOK_URL
+        )
+
+
+@router.on_event("shutdown")
+async def shutdown():
+    await app.session.close()
 
 
 @router.post('/webhook')
