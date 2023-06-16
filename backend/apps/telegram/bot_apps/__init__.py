7e680058a94a90a7a193d1754ab4b74a90d65c49
Mamedov Murad <mamedov_99b@mail.ru>
2023-06-16 09:12:37 +0300

    [FIX]

--- backend/apps/telegram/bot_apps/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Dispatcher
-
-from . import start
-
-
-def init_all_handlers(dp: Dispatcher):
-    start.init_handlers(dp)
--- backend/apps/telegram/bot_apps/start/utils.py
+++
@@ -1,7 +0,0 @@
-from apps.telegram.models import TelegramAppsType
-from apps.telegram.utils import TelegramTextFactory
-
-
-# ADD cache function
-async def get_app_texts():
-    return await TelegramTextFactory.get(typ=TelegramAppsType.START)
