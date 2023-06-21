0f0f76e7bdd6aaaab0973dc333e5d855d6c2fa00
Mamedov Murad <mamedov_99b@mail.ru>
2023-06-21 14:50:51 +0300

    [FIX]

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    password = models.CharField(_('Password'), max_length=128, blank=True, null=True, default=None)
+
     chat_id = models.BigIntegerField(_('Chat id'), blank=True, null=True, default=None)
 
     balance = models.DecimalField(_('Balance'), max_digits=25, decimal_places=25, default=0)
