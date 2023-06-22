3addf642be3c4b0d7e024d3591c70a5388543102
Mamedov Murad <mamedov_99b@mail.ru>
2023-06-22 13:58:41 +0300

    [ADD] cryptocurrencies factories

---
+++ backend/apps/cryptocurrencies/tests/factories.py
@@ -0,0 +1,42 @@
+import factory.fuzzy
+import faker
+from factory.django import DjangoModelFactory
+
+from apps.cryptocurrencies import models
+
+fake = faker.Faker()
+
+
+class NetworkFactory(DjangoModelFactory):
+    class Meta:
+        model = models.Network
+
+    name = factory.Sequence(lambda n: "name#%d" % n)
+    chain_id = factory.fuzzy.FuzzyInteger(low=1, high=10**2)
+    url = fake.unique.url()
+    active = True
+
+
+class CurrencyFactory(DjangoModelFactory):
+    class Meta:
+        model = models.Currency
+
+    name = factory.Sequence(lambda n: "name#%d" % n)
+    symbol = factory.Sequence(lambda n: "symbol#%d" % n)
+    decimal_place = factory.fuzzy.FuzzyInteger(low=6, high=25)
+    address = None
+    network = factory.SubFactory(NetworkFactory)
+    active = True
+
+    class Params:
+        stable_coin = factory.Trait(
+            address=factory.Sequence(lambda n: "address#%d" % n),
+        )
+
+
+class ProviderFactory(DjangoModelFactory):
+    class Meta:
+        model = models.Provider
+
+    address = None
+    network = factory.Sequence(lambda n: "address#%d" % n)
