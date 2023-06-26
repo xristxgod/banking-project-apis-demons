d4587b126357a5ca9f705d01b8ae37ea58b48ec7
Mamedov Murad <mamedov_99b@mail.ru>
2023-06-26 12:19:58 +0300

    [FIX] user.chat_id => user.id pk [ADD] TextParamsMiddleware [ADD] BaseUserData => AnonymousUserData, UserData [ADD] new AbstractHandler [FIX] other handlers

--- backend/apps/telegram/bot_apps/base/handlers.py
+++ backend/apps/telegram/bot_apps/base/handlers.py
@@ -3,36 +3,69 @@ from typing import Optional
 
 import telebot
 from telebot import types
+from telebot.types import Message, CallbackQuery
 
-from apps.telegram.bot_apps.middlewares import UserData
+from apps.telegram.bot_apps.middlewares import BaseUserData
 
 
 class AbstractHandler(metaclass=abc.ABCMeta):
+    class IncorrectTextParams(Exception):
+        pass
+
+    @classmethod
+    def _has_text_params(cls, call: Message | CallbackQuery):
+        return isinstance(call, Message) and len(call.text.split())
 
     def __init__(self, bot: telebot.TeleBot):
         self.bot = bot
         self.registration_handlers()
 
-    def __call__(self, msg_or_cq: types.Message | types.CallbackQuery, data: dict):
-        from_user = msg_or_cq.from_user
+    @abc.abstractmethod
+    def registration_handlers(self): ...
 
-        if isinstance(msg_or_cq, types.Message):
-            message_id, cb_data, message = None, None, msg_or_cq
+    @property
+    def use_text_params(self) -> bool:
+        return False
+
+    @property
+    def use_auth(self) -> bool:
+        return True
+
+    def _pre_call(self, message: Message, user: BaseUserData, **kwargs) -> dict:
+        if self.use_auth and user.is_anonymous:
+            return self.call_without_auth(message, user, kwargs['cb_data'])
+        elif self.use_text_params and kwargs['text_params']:
+            return self.call_with_text_params(message, user, kwargs['text_params'])
         else:
-            message_id, cb_data, message = msg_or_cq.message.message_id, msg_or_cq.data, msg_or_cq.message
+            return self.call(message, user, kwargs['cb_data'])
 
-        if self.has_auth and data['user'] is None:
-            params = self.without_auth_call(message=message, from_user=from_user, cb_data=cb_data)
-        else:
-            params = self.call(message=message, user=data['user'], cb_data=cb_data)
+    def __call__(self, call: Message | CallbackQuery, data: dict):
+        message, cb_data, message_id = getattr(call, 'message', call), getattr(call, 'data', None), None
+        if isinstance(call, CallbackQuery):
+            message_id = message.message_id
 
-        self.notify(
-            chat_id=from_user.id,
-            message_id=message_id,
-            **params,
+        params = self._pre_call(
+            message=message,
+            user=data['user'],
+            cb_data=cb_data,
+            text_params=data.get('text_params'),
         )
 
-    def notify(self, chat_id: int, message_id: Optional[int] = None, **params):
+        self._make_call(
+            chat_id=call.from_user.id,
+            message_id=message_id,
+            **params
+        )
+
+    @abc.abstractmethod
+    def call_without_auth(self, message: types.Message, user: BaseUserData, cb_data: str) -> dict: ...
+
+    @abc.abstractmethod
+    def call(self, message: types.Message, user: BaseUserData, cb_data: str) -> dict: ...
+
+    def call_with_text_params(self, message: types.Message, user: BaseUserData, text_params: list) -> dict: ...
+
+    def _make_call(self, chat_id: int, message_id: Optional[int] = None, **params):
         if message_id:
             self.bot.edit_message_text(
                 chat_id=chat_id,
@@ -44,16 +77,3 @@ class AbstractHandler(metaclass=abc.ABCMeta):
                 chat_id=chat_id,
                 **params,
             )
-
-    @property
-    def has_auth(self) -> bool:
-        return True
-
-    @abc.abstractmethod
-    def registration_handlers(self): ...
-
-    @abc.abstractmethod
-    def call(self, message: types.Message, user: UserData, cb_data: str) -> dict: ...
-
-    @abc.abstractmethod
-    def without_auth_call(self, message: types.Message, from_user: types.User, cb_data: str) -> dict: ...
--- backend/apps/telegram/bot_apps/middlewares.py
+++ backend/apps/telegram/bot_apps/middlewares.py
@@ -3,20 +3,38 @@ import decimal
 from telebot import types
 from telebot.handler_backends import BaseMiddleware
 
-from apps.users.models import User
+from apps.users import models
 
 
-class UserData:
-    def __init__(self, obj: User):
+class BaseUserData:
+    def __init__(self, obj: types.User | models.User):
         self.obj = obj
 
     @property
+    def is_anonymous(self) -> bool:
+        raise NotImplementedError()
+
+    @property
     def chat_id(self) -> int:
-        return self.obj.chat_id
+        return self.obj.id
+
+    id = chat_id
 
     @property
     def username(self) -> str:
         return self.obj.username
+
+
+class AnonymousUserData(BaseUserData):
+    @property
+    def is_anonymous(self) -> bool:
+        return True
+
+
+class UserData(AnonymousUserData):
+    @property
+    def is_anonymous(self) -> bool:
+        return False
 
     @property
     def balance(self) -> decimal.Decimal:
@@ -29,16 +47,30 @@ class UserMiddleware(BaseMiddleware):
     def __init__(self):
         self.update_types = ['message', 'callback_query']
 
-    def pre_process(self, msg_or_cq: types.Message | types.CallbackQuery, data: dict):
-        qs = User.objects.filter(chat_id=msg_or_cq.from_user.id)
+    def pre_process(self, call: types.Message | types.CallbackQuery, data: dict):
+        qs = models.User.objects.filter(id=call.from_user.id)
 
-        user = None
         if qs.exists():
-            user = UserData(
-                obj=qs.first()
-            )
+            user = UserData(obj=qs.first())
+        else:
+            user = AnonymousUserData(call.from_user)
 
         data['user'] = user
 
-    def post_process(self, msg_or_cq: types.Message, data: dict, exception=None):
+    def post_process(self, call: types.Message, data: dict, exception=None):
         del data['user']
+
+
+class TextParamsMiddleware(BaseMiddleware):
+    def __init__(self):
+        self.update_types = ['message']
+
+    def pre_process(self, message: types.Message, data: dict):
+        text_params = None
+        if len(message.text.split()) > 1:
+            text_params = message.text.split()[1:]
+
+        data['text_params'] = text_params
+
+    def post_process(self, call: types.Message, data: dict, exception=None):
+        del data['text_params']
