5fc38aea6dd7266401c92fdcc0bb155e844ca61a
Mamedov Murad <mamedov_99b@mail.ru>
2023-06-26 12:21:46 +0300

    [FIX]

--- backend/apps/telegram/bot_apps/orders/handlers.py
+++ backend/apps/telegram/bot_apps/orders/handlers.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext as _
 from apps.telegram.bot_apps.base.keyboards import get_back_button
 from apps.telegram.bot_apps.start.handlers import StartHandler
 from apps.telegram.bot_apps.utils import make_text
-from apps.telegram.bot_apps.middlewares import UserData
+from apps.telegram.bot_apps.middlewares import BaseUserData
 from apps.telegram.bot_apps.orders import keyboards
 
 
@@ -21,7 +21,7 @@ class OrderHandler(StartHandler):
             func=lambda call: call.data == 'orders',
         )
 
-    def call(self, message: types.Message, user: UserData, cb_data: str) -> dict:
+    def call(self, message: types.Message, user: BaseUserData, cb_data: str) -> dict:
         markup = keyboards.get_orders_keyboard()
         if cb_data:
             markup.row(get_back_button(pattern='menu'))
