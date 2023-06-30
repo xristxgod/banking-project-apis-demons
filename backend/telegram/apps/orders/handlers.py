0e88f6490248bfe52b4fbcc2478cd2d5be432637
Mamedov Murad <mamedov_99b@mail.ru>
2023-06-30 12:56:56 +0300

    [ADD] orders handler

---
+++ backend/telegram/apps/orders/handlers.py
@@ -0,0 +1,31 @@
+from django.db import transaction
+from django.utils.translation import gettext as _
+
+from telegram.utils import make_text
+from apps.telegram.bot_apps.base.keyboards import get_back_keyboard
+from telegram.middlewares.request import TelegramRequest
+from telegram.apps.start.handlers import StartHandler
+
+from telegram.apps.orders import keyboards
+
+
+class OrdersHandler(StartHandler):
+    def attach(self):
+        self.bot.register_message_handler(
+            callback=self,
+            commands=['orders', 'order'],
+        )
+        self.bot.register_callback_query_handler(
+            callback=self,
+            func=lambda call: call.data == 'orders',
+        )
+
+    def call(self, request: TelegramRequest) -> dict:
+        markup = keyboards.get_orders_keyboard()
+        if request.data:
+            markup.row(get_back_keyboard('menu'))
+
+        return dict(
+            text=make_text(_(':upwards_button: Select actions: :downwards_button:')),
+            reply_markup=markup,
+        )
---
+++ backend/telegram/apps/orders/keyboards.py
@@ -0,0 +1,21 @@
+from telebot import types
+
+from django.utils.translation import gettext as _
+
+from apps.telegram.utils import make_text
+
+
+def get_orders_keyboard() -> types.InlineKeyboardMarkup:
+    keyboard = types.InlineKeyboardMarkup()
+
+    keyboard.row(types.InlineKeyboardButton(
+        text=make_text(_(':briefcase: My orders')),
+        callback_data='my_orders',
+    ))
+
+    keyboard.row(types.InlineKeyboardButton(
+        text=make_text(_(':money_with_wings: Deposit')),
+        callback_data='deposit'
+    ))
+
+    return keyboard
