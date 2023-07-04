688515d31e99ec131ae3b0b5880019d47a5fcbf5
Mamedov Murad <mamedov_99b@mail.ru>
2023-07-04 14:14:28 +0300

    [ADD] generate_temp_wallet

---
+++ backend/apps/cryptocurrencies/services.py
@@ -0,0 +1,9 @@
+from apps.cryptocurrencies.models import Currency
+
+
+def generate_temp_wallet(currency: Currency):
+    # TODO
+    return {
+        'address': '...',
+        'private_key': '...',
+    }
