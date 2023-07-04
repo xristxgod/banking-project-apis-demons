0c8aaa3090b77cb61968aa2e209e89a09fc8b2ec
Mamedov Murad <mamedov_99b@mail.ru>
2023-07-04 16:37:18 +0300

    [FIX] get_balance [ADD] cache

--- backend/apps/users/services.py
+++ backend/apps/users/services.py
@@ -1,37 +1,35 @@
 import decimal
-from typing import Optional
 
 from django.db.models import Sum
 
+from src.caches.ram import cached
+
 from apps.users.models import User
-from apps.orders.models import OrderStatus, Deposit
+from apps.orders.models import OrderStatus, Payment
 
 
-# Cached 2 minute and uncache by signal
+@cached(60 * 2)
 def get_balance(user: User) -> decimal.Decimal:
-    balance = 0
-
-    deposits_amount = Deposit.objects.filter(
+    deposit = Payment.objects.filter(
         order__user=user,
         order__status=OrderStatus.DONE,
+        type__in=[Payment.Type.DEPOSIT, Payment.Type.BY_PROVIDER_DEPOSIT]
     ).aggregate(
         Sum('usdt_amount')
     )
 
-    balance += deposits_amount['usdt_amount__sum'] or 0
+    withdraw = Payment.objects.filter(
+        order__user=user,
+        order__status=OrderStatus.DONE,
+        type=Payment.Type.WITHDRAW
+    ).aggregate(
+        Sum('usdt_amount')
+    )
+
+    balance = deposit['usdt_amount__sum'] - withdraw['usdt_amount__sum']
 
     with decimal.localcontext() as ctx:
         ctx.prec = 2
         balance = ctx.create_decimal(balance)
 
     return balance
-
-
-def get_active_deposit(user: User) -> Optional[Deposit]:
-    qs = Deposit.objects.filter(
-        order__user=user,
-        order__status=OrderStatus.CREATED,
-    )
-
-    if qs.exists():
-        return qs.first()
