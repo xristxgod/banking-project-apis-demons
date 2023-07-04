f1bca1dc1fb9ba1e17aa2c1781c22baece76b7b3
Mamedov Murad <mamedov_99b@mail.ru>
2023-07-04 10:49:09 +0300

    [FIX]

--- backend/apps/cryptocurrencies/rest/views.py
+++ backend/apps/cryptocurrencies/rest/views.py
@@ -13,6 +13,34 @@ from apps.cryptocurrencies.rest import serializers
 @extend_schema(
     request=None,
     responses={
+        status.HTTP_200_OK: serializers.NetworkSerializer(),
+    },
+    summary='Network',
+    description='Network',
+)
+def network_view(request, pk: int):
+    obj = get_object_or_404(models.Network, pk=pk)
+    return Response(serializers.NetworkSerializer(obj).data)
+
+
+@api_view(['GET'])
+@extend_schema(
+    request=None,
+    responses={
+        status.HTTP_200_OK: serializers.CurrencySerializer(),
+    },
+    summary='Currency',
+    description='Currency',
+)
+def currency_view(request, pk: int):
+    obj = get_object_or_404(models.Currency, pk=pk)
+    return Response(serializers.CurrencySerializer(obj).data)
+
+
+@api_view(['GET'])
+@extend_schema(
+    request=None,
+    responses={
         status.HTTP_200_OK: serializers.ProviderSerializer(),
     },
     summary='Provider',
