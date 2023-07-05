45e8d244d182cc191989a69fd72a02def1fd3322
Mamedov Murad <mamedov_99b@mail.ru>
2023-07-05 13:19:50 +0300

    [ADD] new ram cache obj

--- backend/src/caches/ram.py
+++ backend/src/caches/ram.py
@@ -1,6 +1,11 @@
-from datetime import timedelta
+import inspect
+import hashlib
+import functools
+import operator
+from datetime import datetime, timedelta
 
-from django.utils import timezone
+from django.db import models
+from django.utils.encoding import smart_str
 
 from src.meta import Singleton
 
@@ -9,32 +14,64 @@ __all__ = (
 )
 
 
-class CashedStorage(metaclass=Singleton):
+class Cached(metaclass=Singleton):
+
+    @classmethod
+    def _vary_on(cls, function, args, kwargs):
+        args_names = inspect.getfullargspec(function)[0]
+        if len(args_names) > 0 and args_names[0] in ('self', 'cls'):
+            args = args[1:]
+            args_names = args_names[1:]
+
+        params = kwargs.copy()
+        for q, arg_value in enumerate(args):
+            try:
+                arg_name = args_names[q]
+            except IndexError:
+                arg_name = 'arg_{}'.format(q)
+            if arg_name not in params:
+                params[arg_name] = arg_value
+
+        result = []
+        for k, v in sorted(params.items(), key=lambda x: x[0]):
+            result.extend([k, v.pk if isinstance(v, models.Model) else v])
+        return result
+
     def __init__(self):
-        self.storage = {}
+        self._storage = {}
 
-    def cached(self, name: str, ttl: float):
+    def get_slot_name(self, function, args, kwargs) -> str:
+        params = self._vary_on(function, args, kwargs)
+        return u':'.join([
+            f'cache-{function.__module__}.{function.__name__}',
+            hashlib.sha256(functools.reduce(
+                operator.add,
+                map(smart_str, params)
+            ).encode('utf-8')).hexdigest() if params else ''
+        ])
+
+    def cached(self, ttl: float | int):
         def decorator(function):
-            async def wrapper(*args, **kwargs):
-                index = name.format(*args, **kwargs)
-                now = timezone.now()
-                if index in self.storage.keys():
-                    is_yesterday = self.storage[index]['time'].day < now.day
-                    if not is_yesterday or self.storage[index]['time'] + self.storage[index]['ttl'] < now:
-                        return self.storage[index]['value']
+            def wrapper(*args, **kwargs):
+                key = self.get_slot_name(function, args, kwargs)
+                result, t = self._storage.get(key, (None, datetime.min))
 
-                value = await function(*args, **kwargs)
-                self.storage[index] = {
-                    'time': now,
-                    'ttl': timedelta(seconds=ttl),
-                    'value': value
-                }
-                return value
+                if datetime.now() > t + timedelta(seconds=ttl):
+                    self._storage.pop(key, None)
+                    result = None
+
+                if result is None:
+                    result = function(*args, **kwargs)
+                    self._storage[key] = (result, datetime.now())
+
+                return result
+
             return wrapper
+
         return decorator
 
-    def __call__(self, name: str, ttl: float):
-        return self.cached(name=name, ttl=ttl)
+    def __call__(self, ttl: int):
+        return self.cached(ttl=ttl)
 
 
-cached = CashedStorage()
+cached = Cached()
