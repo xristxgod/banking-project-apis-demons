0cba8a2ae03f4283be42b4026c1bd903239bf9e5
Mamedov Murad <mamedov_99b@mail.ru>
2023-07-07 14:41:18 +0300

    [FIX]

--- backend/telegram/tests/tests_services.py
+++ backend/telegram/tests/tests_services.py
@@ -3,11 +3,11 @@ import pytest
 
 @pytest.mark.django_db
 def test_save_message():
+    from django.contrib.contenttypes.models import ContentType
+
     from telegram.models import MessageIDS
     from telegram.services import save_message
     from apps.orders.tests.factories import PaymentFactory
-    from apps.orders.models import Payment
-    from django.contrib.contenttypes.models import ContentType
 
     payment = PaymentFactory(is_created=True)
     message_ids = [1, 2, 3, 5]
