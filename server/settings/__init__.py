677247ef14aefd3f5c816638ea94d1f9397f2c3b
Mamedov Murad <mamedov_99b@mail.ru>
2023-09-08 09:59:57 +0300

    [ADD]test  settings

--- server/settings/__init__.py
+++ server/settings/__init__.py
@@ -1,3 +1,7 @@
 from __future__ import absolute_import
 
 from .common import *
+
+
+if NETWORK == 'TEST':
+    from .test import *
--- server/settings/common/settings.py
+++ server/settings/common/settings.py
@@ -2,4 +2,6 @@ from __future__ import absolute_import
 
 import os
 
+NETWORK = os.getenv('NETWORK', 'COMMON')
+
 DATABASE_URL = os.getenv('DATABASE_URL')
