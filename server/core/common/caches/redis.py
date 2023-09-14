4daabb498e34dbe42a75835d444dd49de7b763ce
Mamedov Murad <mamedov_99b@mail.ru>
2023-09-14 15:31:30 +0300

    [FIX]

--- server/core/common/caches/redis.py
+++ server/core/common/caches/redis.py
@@ -2,50 +2,42 @@ import json
 from typing import Any
 from datetime import datetime, timedelta
 
-import redis
-import aioredis
-
 import settings
+from config.redis import RedisConnector
 from core.common.caches.ram import Cache
 
 
 class BaseCache(Cache):
     uri: str
 
-    __slots__ = (
-        '_connection',
-        '_sync_connection'
-    )
-
     def setup(self):
-        self._connection = aioredis.from_url(self.uri)
-        self._sync_connection = redis.from_url(self.uri)
+        self._storage = RedisConnector(uri=self.uri)
 
     def _sync_get_actual_result(self, key: str, ttl: int | float) -> tuple:
         result, t = (None, datetime.min)
-        if value := self._sync_connection.get(key):
+        if value := self._storage.sync_get(key):
             result, t = json.loads(value)
 
         if datetime.now() > t + timedelta(seconds=ttl):
-            self._sync_connection.delete(key)
+            self._storage.sync_delete(key)
             result = None
         return result, t
 
     async def _async_get_actual_result(self, key: str, ttl: int | float) -> tuple:
         result, t = (None, datetime.min)
-        if value := await self._connection.get(key):
+        if value := await self._storage.async_get(key):
             result, t = json.loads(value)
 
         if datetime.now() > t + timedelta(seconds=ttl):
-            await self._connection.delete(key)
+            await self._storage.async_delete(key)
             result = None
         return result, t
 
     def _sync_set_actual_result(self, key: str, result: Any):
-        self._sync_connection.set(key, json.dumps(result, default=str))
+        self._storage.sync_set(key, json.dumps(result, default=str))
 
     async def _async_set_actual_result(self, key: str, result: Any):
-        await self._connection.set(key, json.dumps(result, default=str))
+        await self._storage.async_set(key, json.dumps(result, default=str))
 
 
 class DefaultCached(BaseCache):
