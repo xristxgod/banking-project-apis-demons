99082cb9980c566de8f2fbb654cc212beaf1d1f9
Mamedov Murad <mamedov_99b@mail.ru>
2023-09-14 15:34:31 +0300

    [ADD] block storage

--- server/core/blockchain/storages.py
+++ server/core/blockchain/storages.py
@@ -1,12 +1,16 @@
 from typing import Optional
 
+import settings
+from config.redis import RedisConnector
+
 
 class BlockNumberStorage:
     def __init__(self, storage_name: str):
-        pass
+        self._key = storage_name
+        self._storage = RedisConnector(uri=settings.DAEMON_STORAGE_BACKEND_URL)
 
     async def set(self, block_number: int):
-        pass
+        await self._storage.async_set(key=self._storage, value=block_number)
 
     async def get(self) -> Optional[int]:
-        pass
+        return await self._storage.async_get(key=self._storage)
