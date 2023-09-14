ef7cd8dfdf855aaa0766ad2e3dc8a105e0f26d23
Mamedov Murad <mamedov_99b@mail.ru>
2023-09-14 10:38:31 +0300

    [ADD] dynamic_db_query_handler [ADD] get_tables [ADD] has_table
    [ADD] RawCRUD.has_table
    [ADD] RawCRUD.raw_exists
    [ADD] BaseDAO.exists

--- server/config/database.py
+++ server/config/database.py
@@ -1,3 +1,6 @@
+import functools
+from typing import Callable, Optional
+
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.ext.asyncio.session import AsyncSession, async_sessionmaker
 from sqlalchemy.ext.asyncio.engine import AsyncEngine, create_async_engine
@@ -21,7 +24,7 @@ extra_session_maker = {
 def db_query_handler(db: str = 'default'):
     _session_maker = session_maker if db == 'default' else extra_session_maker[db]
 
-    def decorator(func):
+    def decorator(func: Callable):
         async def wrapper(*args, **kwargs):
             if kwargs.get('session'):
                 return await func(*args, **kwargs)
@@ -35,3 +38,48 @@ def db_query_handler(db: str = 'default'):
         return wrapper
 
     return decorator
+
+
+def dynamic_db_query_handler(func: Callable):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        if kwargs.get('session'):
+            return await func(*args, **kwargs)
+
+        if args and hasattr(args[0], 'db'):
+            db = getattr(args[0], 'db')
+        elif kwargs.get('cls') and hasattr(kwargs['cls'], 'db'):
+            db = getattr(kwargs['cls'], 'db')
+        elif kwargs.get('self') and hasattr(kwargs['self'], 'db'):
+            db = getattr(kwargs['cls'], 'db')
+        else:
+            db = 'default'
+
+        _session_maker = session_maker if db == 'default' else extra_session_maker[db]
+        async with _session_maker() as session:
+            try:
+                kwargs.update({
+                    'session': session,
+                })
+                return await func(*args, **kwargs)
+            except Exception as err:
+                await session.rollback()
+                raise err
+
+    return wrapper
+
+
+async def get_tables(e: Optional[AsyncEngine] = None):
+    from sqlalchemy import inspect
+    e = e or engine
+    async with e.connect() as connection:
+        tables = await connection.run_sync(
+            lambda sync_conn: inspect(sync_conn).get_table_names()
+        )
+    return tables
+
+
+async def has_table(table_name: str, e: Optional[AsyncEngine] = None):
+    e = e or engine
+    tables = await get_tables(e=e)
+    return table_name in tables
--- server/core/common/dao.py
+++ server/core/common/dao.py
@@ -4,7 +4,7 @@ from typing import TypeVar, Optional
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from config.database import Base, db_query_handler
+from config.database import Base, dynamic_db_query_handler, has_table
 
 ModelType = TypeVar('ModelType', bound=Base)
 
@@ -14,14 +14,27 @@ class RawCRUD(metaclass=abc.ABCMeta):
     db: str = 'default'
 
     @classmethod
-    @db_query_handler(db=db)
+    @dynamic_db_query_handler
+    async def has_table(cls, session: AsyncSession) -> bool:
+        table_name = getattr(cls.model, '__tablename__', None) or cls.model.name
+        return await has_table(table_name=table_name, e=session.bind)
+
+    @classmethod
+    @dynamic_db_query_handler
+    async def raw_exists(cls, filters: list, session: AsyncSession):
+        query = select(cls.model).where(*filters).exists().select()
+        qs = await session.execute(query)
+        return qs.scalar()
+
+    @classmethod
+    @dynamic_db_query_handler
     async def raw_get(cls, filters: list, session: AsyncSession):
         query = select(cls.model).where(*filters).limit(1)
         qs = await session.execute(query)
         return qs.scalar()
 
     @classmethod
-    @db_query_handler(db=db)
+    @dynamic_db_query_handler
     async def raw_filter(cls, filters: list, session: AsyncSession, order_by: Optional[list] = None,
                          limit: Optional[int] = None, offset: Optional[int] = None):
         query = select(cls.model).where(*filters)
@@ -38,7 +51,7 @@ class RawCRUD(metaclass=abc.ABCMeta):
         return result.scalars()
 
     @classmethod
-    @db_query_handler(db=db)
+    @dynamic_db_query_handler
     async def raw_create(cls, obj: model, session: AsyncSession, auto_commit: bool = True):
         session.add(obj)
         if auto_commit:
@@ -46,7 +59,7 @@ class RawCRUD(metaclass=abc.ABCMeta):
         return obj
 
     @classmethod
-    @db_query_handler(db=db)
+    @dynamic_db_query_handler
     async def raw_update(cls, obj: model, data: dict, session: AsyncSession, auto_commit: bool = True):
         for column, value in data.items():
             setattr(obj, column, value)
@@ -56,7 +69,7 @@ class RawCRUD(metaclass=abc.ABCMeta):
         return obj
 
     @classmethod
-    @db_query_handler(db=db)
+    @dynamic_db_query_handler
     async def raw_delete(cls, obj: model, session: AsyncSession, auto_commit: bool = True):
         await session.delete(obj)
         if auto_commit:
@@ -76,6 +89,10 @@ class BaseDAO(RawCRUD, metaclass=abc.ABCMeta):
     @classmethod
     async def all(cls, *, session: Optional[AsyncSession] = None) -> list[model]:
         return await cls.raw_filter(filters=[], session=session)
+
+    @classmethod
+    async def exists(cls, filters: list, *, session: Optional[AsyncSession] = None) -> bool:
+        return await cls.raw_exists(filters=filters, session=session)
 
     @classmethod
     async def filter(cls, filters: list, *, limit: Optional[int] = None,
