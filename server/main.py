e25ced4610998c23571994bf220c2ba7f8434004
Mamedov Murad <mamedov_99b@mail.ru>
2023-09-19 10:28:16 +0300

    [ADD] admin

--- server/main.py
+++ server/main.py
@@ -1,3 +1,25 @@
 import fastapi
+from sqladmin import Admin
 
-app = fastapi.FastAPI()
+from config.database import engine, extra_engines
+
+app = fastapi.FastAPI(
+    title='Merchant',
+)
+
+admin = Admin(
+    app=app,
+    engine=engine,
+    title='Merchant Admin',
+    templates_dir='./templates',
+    authentication_backend=None,    # TODO
+)
+
+admin_exchange_rate = Admin(
+    app=app,
+    engine=extra_engines['exchange-rate'],
+    title='Exchange Rate',
+    templates_dir='./templates',
+    authentication_backend=None,    # TODO
+)
+
